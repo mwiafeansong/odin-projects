ac8c8d4ce7e65ea8d7fde0cd593f007a23647fc2
mwiafeansong <91784976+mwiafeansong@users.noreply.github.com>
2023-01-06 18:22:21 +0000

    Add another paragraph to footer

--- mr-tees-kitchen/src/footer.js
+++ mr-tees-kitchen/src/footer.js
@@ -1,16 +1,20 @@
 export default function footer() {
     const footer = document.createElement('footer');
 
-    const footerPara = document.createElement('p');
-    footerPara.textContent = 'Inspired by Mr. Tee & Jollof.com. Created by mwiafeansong for ';
+    const footerPara1 = document.createElement('p');
+    footerPara1.textContent = 'Inspired by Mr. Tee & Jollof.Com';
+
+    const footerPara2 = document.createElement('p');
+    footerPara2.textContent = 'Created by mwiafeansong for ';
     
     const footerAnchor = document.createElement('a');
     footerAnchor.setAttribute('href', 'https://theodinproject.com');
     footerAnchor.textContent = 'The Odin Project';
     
-    footerPara.appendChild(footerAnchor);
+    footerPara2.appendChild(footerAnchor);
 
-    footer.appendChild(footerPara)
+    footer.appendChild(footerPara1);
+    footer.appendChild(footerPara2);
 
     return footer;
 }
